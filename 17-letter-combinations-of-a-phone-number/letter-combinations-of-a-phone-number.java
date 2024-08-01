50db4409b28b220c9fc76157dd8cfa623e74510e
Prasad Borkar <112455273+Praz3030@users.noreply.github.com>
2024-08-02 00:46:24 +0530

    Time: 6 ms (13.16%) | Memory: 42.6 MB (5.87%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> ans=new ArrayList<>();
+        if(digits==null || digits.length()==0){
+            return ans;
+        }
+
+        String letters[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+        Queue<String> queue= new LinkedList<>();
+        queue.add("");
+
+        for(char digit:digits.toCharArray()){
+            String letter=letters[digit-'0'];
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                String curr=queue.poll();
+                for(char temp:letter.toCharArray()){
+                    queue.add(curr+temp);
+                }
+            }
+        }
+        ans.addAll(queue);
+        return ans;
+    }
+}

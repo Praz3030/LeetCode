2309ae05d1e001def7ca097feeb345342be870c8
Prasad Borkar <112455273+Praz3030@users.noreply.github.com>
2024-08-10 21:46:38 +0530

    Time: 2 ms (89.17%) | Memory: 44.8 MB (39.46%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,61 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List<List<String>> ans = new ArrayList<List<String>>();
+        dfs(0, board, ans);
+        return ans;
+    }
+
+    static boolean validate(char[][] board, int row, int col) {
+        int duprow = row;
+        int dupcol = col;
+        while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+        while (col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+        while (col >= 0 && row < board.length) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+            row++;
+        }
+        return true;
+    }
+
+    static void dfs(int col, char[][] board, List<List<String>> ans) {
+        if (col == board.length) {
+            ans.add(construct(board));
+            return;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (validate(board, row, col)) {
+                board[row][col] = 'Q';
+                dfs(col + 1, board, ans);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    static List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<String>();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+}

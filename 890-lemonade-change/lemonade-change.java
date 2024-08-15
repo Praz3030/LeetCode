648e9b2263f2c6c6b4cf27a766a7ac92c44eaa4e
Prasad Borkar <112455273+Praz3030@users.noreply.github.com>
2024-08-15 13:12:58 +0530

    Time: 1 ms (100.00%) | Memory: 56 MB (27.95%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for ( int bill : bills ) {
+
+            if ( bill == 5 ) {
+                five++;
+            }
+            else if ( bill == 10 ) {
+
+                if ( five > 0){
+                    ten++;
+                    five--;
+                }
+                else return false;
+            }
+            else if ( bill == 20 ) {
+
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                else return false;
+            }
+        }
+
+        return true;
+    }
+}

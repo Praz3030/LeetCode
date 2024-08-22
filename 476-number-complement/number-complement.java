0ae95b59fad0c7c96fdb367beb6bdef861ea6166
Prasad Borkar <112455273+Praz3030@users.noreply.github.com>
2024-08-22 20:39:54 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (51.69%) - LeetSync

---
+++ 476-number-complement/number-complement.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findComplement(int num) {
+
+        int len = Integer.toBinaryString(num).length();
+        
+        int mask = (1 << len) - 1;
+
+        return num ^ mask;
+    }
+}

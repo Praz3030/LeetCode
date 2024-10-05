b0760e1fc2973ea54edaf987d7bf4244223cc78a
Prasad Borkar <112455273+Praz3030@users.noreply.github.com>
2024-10-05 22:03:55 +0530

    Time: 2 ms (73.65%) | Memory: 42 MB (11.36%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,11 +1,11 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         char diff = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
             diff ^= s.charAt(i);
         }
-
+        n = t.length();
         for (int i = 0; i < t.length(); i++) {
             diff ^= t.charAt(i);
         }
